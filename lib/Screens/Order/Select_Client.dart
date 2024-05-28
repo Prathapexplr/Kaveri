880606a08a9a4930f14170bb83149704163b644d
Prathap <prathapraja2k@gmail.com>
2024-05-28 22:43:48 +0530

    All Client List API

--- lib/Screens/Order/Select_Client.dart
+++ lib/Screens/Order/Select_Client.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'package:kaveri/Screens/Order/Selected_Client.dart';
 
 class SelectClientScreen extends StatefulWidget {
@@ -17,102 +20,131 @@ class SelectClientScreen extends StatefulWidget {
 }
 
 class _SelectClientScreenState extends State<SelectClientScreen> {
+  List<dynamic> clients = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchClients();
+  }
+
+  Future<void> fetchClients() async {
+    final response =
+        await http.get(Uri.parse('https://api.jaynaturals.com/clients'));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonResponse = json.decode(response.body);
+      setState(() {
+        clients = jsonResponse[
+            'clients']; // Adjust this based on the actual JSON structure
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load clients');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 230, 229, 229),
-        body: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: [
-              Container(
-                height: 40,
-                width: width / 1,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Icon(Icons.search),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 12),
-                        child: TextField(
-                          decoration: const InputDecoration(
-                            // hintText: 'Search...',
-                            border: InputBorder.none,
-                          ),
-                          onChanged: (value) {},
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 229, 229),
+      body: Padding(
+        padding: EdgeInsets.only(top: height / 20),
+        child: Column(
+          children: [
+            Container(
+              height: 40,
+              width: width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Icon(Icons.search),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 12),
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SelectedClientScreen(
-                            employeeId: widget.employeeId,
-                            firstName: widget.firstName,
-                            profilePicture: widget.profilePicture)),
-                  );
-                },
-                child: Container(
-                  height: 130,
-                  width: width / 1,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.white),
-                  child: Container(
-                    child: const Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: Column(
-                        // mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Saranava hardware",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            "no 113, otrai vadai street, Tiru Vi Ka Nagar, Perambur, Chennai, Tamil Nadu 600110",
-                            style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black),
-                          ),
-                          SizedBox(
-                            height: 25,
-                          ),
-                          Text(
-                            "Number:   +91 9551507145",
-                            style: TextStyle(
-                                fontSize: 10, fontWeight: FontWeight.w600),
-                          ),
-                        ],
+                        onChanged: (value) {},
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            isLoading
+                ? const CircularProgressIndicator()
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: clients.length,
+                      itemBuilder: (context, index) {
+                        final client = clients[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SelectedClientScreen(
+                                  employeeId: widget.employeeId,
+                                  firstName: widget.firstName,
+                                  profilePicture: widget.profilePicture,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 5),
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  client['clientName'],
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  client['address'],
+                                  style: const TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                const SizedBox(height: 25),
+                                Text(
+                                  "Number:   ${client['contactNumber']}",
+                                  style: const TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+          ],
         ),
       ),
     );
